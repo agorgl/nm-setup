2b882b20dee12144476e0d21a0f8f79c1982763d
Agorgianitis Loukas <agorglouk@gmail.com>
2022-09-23 13:21:01 +0300

    Replace priviledged flag with more specific capabilities and sysctl flags

--- scripts/nm-join.sh
+++ scripts/nm-join.sh
@@ -10,8 +10,15 @@ SERVER=$(echo $TOKEN | base64 -d | jq -r .apiconnstring)
 openssl s_client -showcerts -connect $SERVER </dev/null 2>/dev/null | openssl x509 -outform PEM > $CERT_FILE
 
 # Launch netclient
-podman run -d --privileged --name netclient-$(openssl rand -hex 4) \
+podman run -d --name netclient-$(openssl rand -hex 4) \
     -v $CERT_FILE:/selfsigned.pem \
     -e SSL_CERT_FILE=/selfsigned.pem \
     -e TOKEN=$TOKEN \
+    --cap-add NET_ADMIN \
+    --cap-add NET_RAW \
+    --cap-add SYS_MODULE \
+    --sysctl net.ipv4.ip_forward=1 \
+    --sysctl net.ipv4.conf.all.src_valid_mark=1 \
+    --sysctl net.ipv6.conf.all.disable_ipv6=0 \
+    --sysctl net.ipv6.conf.all.forwarding=1 \
     gravitl/netclient:latest
