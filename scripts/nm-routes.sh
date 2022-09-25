9cd0ae794e488475e4b9a6afb486498bd5274101
Agorgianitis Loukas <agorglouk@gmail.com>
2022-09-25 18:01:51 +0300

    Support route propagation from multiple containers

--- scripts/nm-routes.sh
+++ scripts/nm-routes.sh
@@ -3,15 +3,23 @@
 # Constants
 ROUTING_TABLE=5050
 
-# Get container name, ip and routes
-container=$(podman ps --format '{{ .Names }}' | grep 'netmaker-\|netclient-' | head -n 1)
-container_ip=$(podman inspect $container -f '{{ .NetworkSettings.IPAddress }}')
-routes=$(podman exec $container ip route | grep nm- | awk '{print $1}' | grep '/')
+# Gather applicable containers
+containers=$(podman ps --format '{{ .Names }}' | grep 'netmaker-server\|netclient-')
 
-# Add them to custom routing table
-ip route flush table $ROUTING_TABLE
-printf "%s\n" $routes | xargs -I {} ip route replace {} via $container_ip table $ROUTING_TABLE
+# For each container
+for container in $containers; do
+    # Get container ip and routes
+    echo "Adding routes for container $container"
+    container_ip=$(podman inspect $container -f '{{ .NetworkSettings.IPAddress }}')
+    routes=$(podman exec $container ip route | grep nm- | awk '{print $1}' | grep '/')
 
-# Add rule to custom routing table
-ip rule list | awk '/lookup/ {print $NF}' | grep $ROUTING_TABLE | xargs ip rule del table
-ip rule add from all table $ROUTING_TABLE
+    # Add them to custom routing table
+    echo "Adding routes for networks $(echo $routes) via container ip $container_ip in table $ROUTING_TABLE"
+    ip route flush table $ROUTING_TABLE
+    printf "%s\n" $routes | xargs -I {} ip route replace {} via $container_ip table $ROUTING_TABLE
+
+    # Add rule to custom routing table
+    echo "Adding table $ROUTING_TABLE to routing policy database"
+    ip rule list | awk '/lookup/ {print $NF}' | grep $ROUTING_TABLE | xargs ip rule del table
+    ip rule add from all table $ROUTING_TABLE
+done
