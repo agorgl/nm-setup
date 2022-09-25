c33378c047db2ae619a66431a7e0412e1a19cfe6
Agorgianitis Loukas <agorglouk@gmail.com>
2022-09-25 19:33:42 +0300

    Support differential route sync

--- scripts/nm-routes.sh
+++ scripts/nm-routes.sh
@@ -7,19 +7,40 @@ ROUTING_TABLE=5050
 containers=$(podman ps --format '{{ .Names }}' | grep 'netmaker-server\|netclient-')
 
 # For each container
+all_routes=""
 for container in $containers; do
     # Get container ip and routes
-    echo "Adding routes for container $container"
     container_ip=$(podman inspect $container -f '{{ .NetworkSettings.IPAddress }}')
-    routes=$(podman exec $container ip route | grep nm- | awk '{print $1}' | grep '/')
+    container_routes=$(podman exec $container ip route | grep nm- | awk '{print $1}' | grep '/' | xargs printf "%s:$container_ip\n")
+    all_routes+=$container_routes
+
+    # Get existing routes
+    routes=$(ip route show table $ROUTING_TABLE | awk '{print $1 ":" $3}')
+
+    # Calculate new routes
+    new_routes=$(comm -23 <(printf "%s\n" $container_routes | sort) <(printf "%s\n" $routes | sort))
 
     # Add them to custom routing table
-    echo "Adding routes for networks $(echo $routes) via container ip $container_ip in table $ROUTING_TABLE"
-    ip route flush table $ROUTING_TABLE
-    printf "%s\n" $routes | xargs -I {} ip route replace {} via $container_ip table $ROUTING_TABLE
-
-    # Add rule to custom routing table
-    echo "Adding table $ROUTING_TABLE to routing policy database"
-    ip rule list | awk '/lookup/ {print $NF}' | grep $ROUTING_TABLE | xargs ip rule del table
-    ip rule add from all table $ROUTING_TABLE
+    for r in $new_routes; do
+        route=${r/:/ via }
+        echo "Adding new route $route"
+        ip route add $route table $ROUTING_TABLE
+    done
 done
+
+# Calculate old routes
+routes=$(ip route show table $ROUTING_TABLE | awk '{print $1 ":" $3}')
+old_routes=$(comm -13 <(printf "%s\n" $all_routes | sort | uniq) <(printf "%s\n" $routes | sort))
+
+# Remove old routes from custom routing table
+for r in $old_routes; do
+    route=${r/:/ via }
+    echo "Removing old route $route"
+    ip route del $route table $ROUTING_TABLE
+done
+
+# Add rule to custom routing table
+if [ -z "$(ip rule list | awk '/lookup/ {print $NF}' | grep $ROUTING_TABLE)" ]; then
+    echo "Adding table $ROUTING_TABLE to routing policy database"
+    ip rule add from all table $ROUTING_TABLE
+fi
